816934e6e736ce68f63ca34ee650354f7348fb91
Markus Souza <markus.souza@gmail.com>
2018-04-03 15:26:05 -0300

    Initial commit

---
+++ src/main/java/com/mkstecnologia/zuul/filters/PostFilter.java
@@ -0,0 +1,27 @@
+package com.mkstecnologia.zuul.filters;
+
+import com.netflix.zuul.ZuulFilter;
+
+public class PostFilter extends ZuulFilter {
+
+	@Override
+	public Object run() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+		return "post";
+	}
+
+}
